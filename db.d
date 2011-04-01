4d8efed5d2c9b16e39d02bd11eda75189db70ac6
Piotr Szturmaj <pszturmaj@tlen.pl>
2011-04-01 18:11:08 +0200

    Tabs converted to spaces

--- db.d
+++ db.d
@@ -77,106 +77,106 @@ assert(row.s == "anotherText");
 */
 struct DBRow(Specs...)
 {
-	static if (Specs.length == 0)
-		alias Variant[] T;
-	else static if (Specs.length == 1)
-		alias Specs[0] T;
-	else
-		alias Tuple!Specs T;
-	
-	T base;
-	alias base this;
-	
-	static if (isDynamicArray!T && !isSomeString!T)
-	{
-		alias typeof(T[0]) ElemType;
-		enum hasStaticLength = false;
+    static if (Specs.length == 0)
+        alias Variant[] T;
+    else static if (Specs.length == 1)
+        alias Specs[0] T;
+    else
+        alias Tuple!Specs T;
+    
+    T base;
+    alias base this;
+    
+    static if (isDynamicArray!T && !isSomeString!T)
+    {
+        alias typeof(T[0]) ElemType;
+        enum hasStaticLength = false;
 
-		void setLength(size_t length)
-		{
-			base.length = length;
-		}
-		
-		void setNull(size_t index)
-		{
-			static if (isNullable!ElemType)
-				base[index] = null;
-			else
-				throw new Exception("Cannot set NULL to field " ~ to!string(index) ~ " of " ~ T.stringof ~ ", it is not nullable");
-		}
-	}
-	else static if (isCompositeType!T)
-	{
-		static if (isStaticArray!T)
-		{
-			template ArrayTypeTuple(AT : U[N], U, size_t N)
-			{
-				static if (N > 1)
-					alias TypeTuple!(U, ArrayTypeTuple!(U[N - 1])) ArrayTypeTuple;
-				else
-					alias TypeTuple!U ArrayTypeTuple;
-			}
-			
-			alias ArrayTypeTuple!T fieldTypes;
-		}
-		else
-			alias FieldTypeTuple!T fieldTypes;
-		
-		enum hasStaticLength = true;
-		
-		void set(U, size_t index)(U value)
-		{
-			static if (isStaticArray!T)
-				base[index] = value;
-			else
-				base.tupleof[index] = value;
-		}
-		
-		void setNull(size_t index)()
-		{
-			static if (isNullable!(fieldTypes[index]))
-			{
-				static if (isStaticArray!T)
-					base[index] = null;
-				else
-					base.tupleof[index] = null;
-			}
-			else
-				throw new Exception("Cannot set NULL to field " ~ to!string(index) ~ " of " ~ T.stringof ~ ", it is not nullable");
-		}
-	}
-	else static if (Specs.length == 1)
-	{
-		alias TypeTuple!T fieldTypes;
-		enum hasStaticLength = true;
+        void setLength(size_t length)
+        {
+            base.length = length;
+        }
+        
+        void setNull(size_t index)
+        {
+            static if (isNullable!ElemType)
+                base[index] = null;
+            else
+                throw new Exception("Cannot set NULL to field " ~ to!string(index) ~ " of " ~ T.stringof ~ ", it is not nullable");
+        }
+    }
+    else static if (isCompositeType!T)
+    {
+        static if (isStaticArray!T)
+        {
+            template ArrayTypeTuple(AT : U[N], U, size_t N)
+            {
+                static if (N > 1)
+                    alias TypeTuple!(U, ArrayTypeTuple!(U[N - 1])) ArrayTypeTuple;
+                else
+                    alias TypeTuple!U ArrayTypeTuple;
+            }
+            
+            alias ArrayTypeTuple!T fieldTypes;
+        }
+        else
+            alias FieldTypeTuple!T fieldTypes;
+        
+        enum hasStaticLength = true;
+        
+        void set(U, size_t index)(U value)
+        {
+            static if (isStaticArray!T)
+                base[index] = value;
+            else
+                base.tupleof[index] = value;
+        }
+        
+        void setNull(size_t index)()
+        {
+            static if (isNullable!(fieldTypes[index]))
+            {
+                static if (isStaticArray!T)
+                    base[index] = null;
+                else
+                    base.tupleof[index] = null;
+            }
+            else
+                throw new Exception("Cannot set NULL to field " ~ to!string(index) ~ " of " ~ T.stringof ~ ", it is not nullable");
+        }
+    }
+    else static if (Specs.length == 1)
+    {
+        alias TypeTuple!T fieldTypes;
+        enum hasStaticLength = true;
 
-		void set(T, size_t index)(T value)
-		{
-			base = value;
-		}
-		
-		void setNull(size_t index)()
-		{
-			static if (isNullable!T)
-				base = null;
-			else
-				throw new Exception("Cannot set NULL to " ~ T.stringof ~ ", it is not nullable");
-		}
-	}
-	
-	static if (hasStaticLength)
-	{
-		static pure void checkReceivedFieldCount(int fieldCount)
-		{
-			if (fieldTypes.length != fieldCount)
-				throw new Exception("Received field count is not equal to " ~ T.stringof ~ "'s field count");
-		}
-	}
-	
-	string toString()
-	{
-		return to!string(base);
-	}
+        void set(T, size_t index)(T value)
+        {
+            base = value;
+        }
+        
+        void setNull(size_t index)()
+        {
+            static if (isNullable!T)
+                base = null;
+            else
+                throw new Exception("Cannot set NULL to " ~ T.stringof ~ ", it is not nullable");
+        }
+    }
+    
+    static if (hasStaticLength)
+    {
+        static pure void checkReceivedFieldCount(int fieldCount)
+        {
+            if (fieldTypes.length != fieldCount)
+                throw new Exception("Received field count is not equal to " ~ T.stringof ~ "'s field count");
+        }
+    }
+    
+    string toString()
+    {
+        return to!string(base);
+    }
 }
 
 /**
@@ -191,32 +191,32 @@ $(UL
 */
 template isCompositeType(T)
 {
-	static if (isTuple!T || is(T == struct) || isStaticArray!T)
-		enum isCompositeType = true;
-	else
-		enum isCompositeType = false;
+    static if (isTuple!T || is(T == struct) || isStaticArray!T)
+        enum isCompositeType = true;
+    else
+        enum isCompositeType = false;
 }
 
 template Nullable(T)
     if (!__traits(compiles, { T t = null; }))
 {
-	/*
-	Currently with void*, because otherwise it wont accept nulls.
-	VariantN need to be changed to support nulls without using void*, which may
-	be a legitimate type to store, as pointed out by Andrei.
-	Preferable alias would be then Algebraic!(T, void) or even Algebraic!T, since
-	VariantN already may hold "uninitialized state".
-	*/
-	alias Algebraic!(T, void*) Nullable;
+    /*
+    Currently with void*, because otherwise it wont accept nulls.
+    VariantN need to be changed to support nulls without using void*, which may
+    be a legitimate type to store, as pointed out by Andrei.
+    Preferable alias would be then Algebraic!(T, void) or even Algebraic!T, since
+    VariantN already may hold "uninitialized state".
+    */
+    alias Algebraic!(T, void*) Nullable;
 }
 
 template isVariantN(T)
 {
-	//static if (is(T X == VariantN!(N, Types), uint N, Types...)) // doesn't work due to BUG 5784
-	static if (T.stringof.length >= 8 && T.stringof[0..8] == "VariantN") // ugly temporary workaround
-		enum isVariantN = true;
-	else
-		enum isVariantN = false;
+    //static if (is(T X == VariantN!(N, Types), uint N, Types...)) // doesn't work due to BUG 5784
+    static if (T.stringof.length >= 8 && T.stringof[0..8] == "VariantN") // ugly temporary workaround
+        enum isVariantN = true;
+    else
+        enum isVariantN = false;
 }
 
 static assert(isVariantN!Variant);
@@ -225,22 +225,22 @@ static assert(isVariantN!(Nullable!int));
 
 template isNullable(T)
 {
-	static if ((isVariantN!T && T.allowed!(void*)) || is(T X == Nullable!U, U))
-		enum isNullable = true;
-	else
-		enum isNullable = false;
+    static if ((isVariantN!T && T.allowed!(void*)) || is(T X == Nullable!U, U))
+        enum isNullable = true;
+    else
+        enum isNullable = false;
 }
 
 static assert(isNullable!Variant);
 static assert(isNullable!(Nullable!int));
 
 template nullableTarget(T)
-	if (isVariantN!T && T.allowed!(void*))
+    if (isVariantN!T && T.allowed!(void*))
 {
-	alias T nullableTarget;
+    alias T nullableTarget;
 }
 
 template nullableTarget(T : Nullable!U, U)
 {
-	alias U nullableTarget;
+    alias U nullableTarget;
 }
