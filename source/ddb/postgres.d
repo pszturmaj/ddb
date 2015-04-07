5f7f786c097749a05a85480d406a41f2ec782b04
bcherian <benjamin.cherian@villanova.edu>
2015-04-07 00:21:27 -0400

    #1 implementing VARCHAR. using same as TEXT

--- source/ddb/postgres.d
+++ source/ddb/postgres.d
@@ -1111,6 +1111,7 @@ class PGConnection
                     case PGType.INT2: checkParam!short(2); break;
                     case PGType.INT4: checkParam!int(4); break;
                     case PGType.INT8: checkParam!long(8); break;
+                    case PGType.VARCHAR:
                     case PGType.TEXT:
                         paramsLen += param.value.coerce!string.length;
                         hasText = true;
@@ -1162,6 +1163,7 @@ class PGConnection
                         stream.write(cast(int)8);
                         stream.write(param.value.coerce!long);
                         break;
+                    case PGType.VARCHAR:
                     case PGType.TEXT:
                         auto s = param.value.coerce!string;
                         stream.write(cast(int) s.length);
